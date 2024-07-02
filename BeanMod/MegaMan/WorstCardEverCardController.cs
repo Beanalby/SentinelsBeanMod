da8a9fa066155dc6d97e0532d1b585b62d106adb
Jason Viers <2023094+Beanalby@users.noreply.github.com>
2024-07-02 18:32:58 -0400

    Initial version, it's a mod that technically works.

---
+++ BeanMod/MegaMan/WorstCardEverCardController.cs
@@ -0,0 +1,18 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System.Collections;
+
+namespace BeanMod.MegaMan
+{
+    class WorstCardEverCardController : CardController
+    {
+        public WorstCardEverCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+        }
+        public override IEnumerator Play()
+        {
+            return this.GameController.SendMessageAction("You knew this card does nothing. Why would you play it?", Priority.Medium, GetCardSource());
+        }
+    }
+}
